85f6b0531c648dc17d22863aad911d553900d6d2
willAtaides <sg552.santos@gmail.com>
2025-07-05 08:43:15 -0300

    get list em Order e Product

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -72,4 +73,22 @@ public class Order {
     public void setClient(User client) {
         this.client = client;
     }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.getProduct()).toList();
+    }
+
+
 }
